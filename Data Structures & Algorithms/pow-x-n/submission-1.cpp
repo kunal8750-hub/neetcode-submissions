3abd853cb3a795ec561ec8f14c714d25f41c3c59
Kunal Kumar <kunalkumarbnps@gmail.com>
2026-06-24 18:14:41 +0530

    Add: pow-x-n - submission-1

---
+++ Data Structures & Algorithms/pow-x-n/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long N = n;
+        if(N<0){
+            x = 1/x;
+            N = -N;
+        }
+        double ans = 1.0;
+        for(long long i=0; i<N; i++){
+            ans = ans*x;
+            
+        }
+        return ans;
+    }
+};
